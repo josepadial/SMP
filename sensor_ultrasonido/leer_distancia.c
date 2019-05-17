019f3d2145520660e51dfc9b4d816df5ea9fe886
JaimeAmate <amate97@gmail.com>
2019-05-17 18:25:00 +0200

    a

--- sensor_ultrasonido/leer_distancia.c
+++ sensor_ultrasonido/leer_distancia.c
@@ -3,7 +3,7 @@
 #define INF_16_BIT 65535
 #define SR04_TRIGG PIN_A4
 #define SR04_ECHO PIN_B3
-#use rs232(baud=9600, bits=8, parity=N,xmit=PIN_C4,rcv=PIN_C5)
+//#use rs232(baud=9600, bits=8, parity=N,xmit=PIN_C4,rcv=PIN_C5)
  
 // 50 cm * (59 ms / 1 cm) * (3uds /2ms) = 4425
 
@@ -22,6 +22,7 @@ void check_time_out(){
 #INT_CCP2
 void capture(){
    if (captura == 0){
+      //led_on();
       set_timer1(0);
       captura++;
       enable_interrupts(INT_TIMER1);   
@@ -36,12 +37,12 @@ void capture(){
 }
 
 void pulseIn_ECHO_HIGH(){
+   v_captura = 0;
    setup_ccp2(CCP_CAPTURE_RE);
    enable_interrupts(INT_CCP2);
    setup_timer_1(T1_INTERNAL|T1_DIV_BY_8);
-   v_captura = 0;
    while(v_captura == 0){
-      //led_on();
+      led_on();
    }
 }
 
@@ -55,14 +56,15 @@ void main(){
       delay_ms(15);
       output_low(SR04_TRIGG);
       pulseIn_ECHO_HIGH();
-      
-      /*if(v_captura < 4425){
+      /*
+      if(v_captura < 4425){
          led_off();
       }
       else{
          led_on();
-      }*/
-      printf("%LX", v_captura);
+      }
+      */
+      
    }
 
 }
