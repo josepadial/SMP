477ef5eb4eb023cedf477b473e7ed8ce90a4e853
Jose Padial <joseismael511@gmail.com>
2019-06-18 13:41:44 +0200

    18/06/2019

--- Comportamiento_Final/final.c
+++ Comportamiento_Final/final.c
@@ -1,11 +1,18 @@
 #include "mismov.h"
 #use delay (clock=48000000)
 
+void inicializarTimer0PWM(){ 
+   setup_timer_0(RTCC_INTERNAL|RTCC_DIV_2|RTCC_8_BIT); 
+   enable_interrupts(INT_TIMER0); 
+   enable_interrupts(GLOBAL); 
+}  
+
 void main() {
+   inicializarTimer0PWM();
    led_on();
    delay_ms(3000);
    led_off();
-   
+   DELANTE();
    while(1){
    
    }
